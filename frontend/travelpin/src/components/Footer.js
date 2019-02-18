6c2b9c65e6ff6734a6f6fc1e75142116e9f455ed
Xi Chen <mol.chen@live.com>
2019-02-18 15:03:09 -0500

    change the strucure of footer

--- frontend/travelpin/src/components/Footer.js
+++ frontend/travelpin/src/components/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import image from '../asserts/images/image.jpeg';
 
 export class Footer extends React.Component {
     render() {
         return(
             <div className="footer">
-            <h1>This is footer</h1>
-        </div>
-    )
+                <div className="row">
+                    <div className="column">
+                        <h2>Column 1</h2>
+                        <p>Some text..</p>
+                    </div>
+                    <div className="column">
+                        <h2>Column 2</h2>
+                        <p>Some text..</p>
+                    </div>
+                    <div className="column">
+                        <h2>Column 3</h2>
+                        <p>Some text..</p>
+                    </div>
+                    <div className="column">
+                        <img src={image}  alt="image" />
+                        <p>Some text..</p>
+                    </div>
+                    <div className="column">
+                        <img src={image}  alt="image" />
+                        <p>Some text..</p>
+                    </div>
+                </div>
+                <div>
+                    <h1>This is section2</h1>
+                </div>
+
+            </div>
+        )
     }
 }
